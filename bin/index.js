22a4ee865136f435c04622c32b306c99a8d2efac
Abhay <abhaybro768@gmail.com>
2023-07-31 17:02:38 +0530

    initial commit

--- bin/index.js
+++ bin/index.js
@@ -1,57 +1,27 @@
-const esbuild = require('esbuild')
-const path = require("path");
-const fs = require("fs")
-const plugin = require('./plugin')
-const { format } = require("prettier")
+const swc = require('@swc/core')
+const fs = require("node:fs/promises");
+const path = require("path")
 
-const root = path.resolve();
-const outdir = path.join(root, '.jsx');
-if (fs.existsSync(outdir)) fs.rmSync(outdir, {
-    recursive: true
-});
-const {ls} = require('./lib/utils.js')
-
-const entryPoints = ls(path.join(root, 'app', 'pages'));
-console.time('html');
-
-(async () => {
+// console.log({module})
+1 && (async () => {
+    // const file = module.filename;
+    const file = path.join(module.path, 'tests', 'main.jsx');
+    const source = (await fs.readFile(file)).toString();
+    console.log({source})
 
     const options = {
-        entryPoints,
-        // jsxFactory: '_createElement',
-        outdir,
-        jsx: 'automatic',
-        // loader: {
-        //     '.js': 'jsx'
-        // },
-        jsxImportSource: path.join(root, 'bin', 'lib'),
-        platform: 'node',
-        target: ['es2020', 'chrome90', 'node20'],
-        format: 'cjs',
-        // format: 'esm',
-
-        plugins: [plugin()],
-        // bundle: true,
-        // external : [path.join(root, 'bin', 'lib', 'jsx-runtime.js')],
+        filename: file,
+        // sourceMaps: true,
+        isModule: true,
+        jsc: {
+            parser: {
+                syntax: "ecmascript",
+                jsx: true,
+            },
+            transform: {},
+        },
     }
-    await esbuild.build(options)
+    const output = await swc.transform(source, options)
 
-    // return
-    // Render the HTML
-    const {
-        default: layout
-    } = require("../.jsx/layout");
-    const html = layout({
-        data: "ABHAY"
-    });
-    console.timeEnd("html")
-    // console.log({ html })
-    const htm = await format(html, {
-        parser: 'html'
-    })
-
-    fs.writeFileSync(path.join(root, '.jsx', 'index.html'), htm);
-    console.log(htm)
-
-
-})()
+    console.log({output})
+})();
